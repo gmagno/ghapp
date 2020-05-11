5fd54588731be5508194f008f1ea05819205cda0
Goncalo <goncalo.magno@gmail.com>
2020-05-11 14:14:11 -0300

    Updates

---
+++ precache-manifest.92893f234f152fb86262a3342d738c8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc01dc7bd1db6097fba46810eb6b273",
+    "url": "/ghapp/index.html"
+  },
+  {
+    "revision": "668c5c3d0e6f2ea72940",
+    "url": "/ghapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f7382e9d549b0c3cf55",
+    "url": "/ghapp/static/js/2.c254a51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghapp/static/js/2.c254a51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668c5c3d0e6f2ea72940",
+    "url": "/ghapp/static/js/main.cc59eefe.chunk.js"
+  },
+  {
+    "revision": "282239c56c71f61cacb2",
+    "url": "/ghapp/static/js/runtime-main.b1f0b3bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
